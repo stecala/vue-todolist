155c8cba775a705d76eb0b9e1c3851e430827711
stecala <ste.cala123@gmail.com>
2022-07-06 12:26:41 +0200

    repo creation

---
+++ script/script.js
@@ -0,0 +1,24 @@
+const app = new Vue({
+    el: '#app',
+    data : {
+        todos: [
+            {
+                text: 'Fare i compiti',
+                done: false
+            },
+            {
+                text: 'Fare la spesa',
+                done: true
+            },
+            {
+                text: 'Fare il bucato',
+                done: false
+            }
+        ],
+
+    },
+    methods : {
+
+    },
+
+})
